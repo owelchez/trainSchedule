e15e25c89e13a4b65e9207e1aa0a6e0a65ca2b7d
Orvin Welchez <owelchez@gmail.com>
2016-06-27 11:36:34 -0400

    HTML work

---
+++ train.js
@@ -0,0 +1,25 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCMdiGQODXX8fIYD6TwnHwudslL68duIHQ",
+    authDomain: "trainschedule-20b45.firebaseapp.com",
+    databaseURL: "https://trainschedule-20b45.firebaseio.com",
+    storageBucket: "trainschedule-20b45.appspot.com",
+  };
+  firebase.initializeApp(config);
+
+
+$('.currentTime').append(moment().format('HH:mm'));
+
+
+
+console.log('You will make it work!');
+
+
+
+
+
+
+
+
+
+
